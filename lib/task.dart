f1c9214b6f40a06653fa9053bc0a8d409668c53a
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-09-11 22:46:27 +0430

    week13 update hive model and recreate adapter

--- lib/task.dart
+++ lib/task.dart
@@ -4,11 +4,14 @@ part 'task.g.dart';
 
 @HiveType(typeId: 3)
 class Task {
-  Task({required this.title, required this.subTitle});
+  Task({required this.title, required this.subTitle, this.isDone = false});
 
   @HiveField(0)
   String title;
 
   @HiveField(1)
   String subTitle;
+
+  @HiveField(2)
+  bool isDone;
 }
