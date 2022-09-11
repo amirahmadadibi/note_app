756c633bda92782ba5895310e557bb3fd3661943
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-09-11 22:31:39 +0430

    week13 E19 show task widget on list

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:msh_checkbox/msh_checkbox.dart';
 import 'package:note_application/student.dart';
 import 'package:note_application/task.dart';
+import 'package:note_application/task_widget.dart';
 
 import 'car.dart';
 
@@ -22,8 +23,6 @@ class _HomeScreenState extends State<HomeScreen> {
   var studnetBox = Hive.box<Student>('studentBox');
   var taskBox = Hive.box<Task>('taskBox');
 
-  bool isChecked = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,117 +32,9 @@ class _HomeScreenState extends State<HomeScreen> {
             itemCount: taskBox.values.length,
             itemBuilder: ((context, index) {
               var task = taskBox.values.toList()[index];
-              return Text(task.title);
+              return TaskWidget(task: task);
             })),
       ),
-    );
-  }
-
-  Container getTaskItem() {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 24),
-      height: 132,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Color(0xffffffff),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: getMainContent(),
-      ),
-    );
-  }
-
-  Row getMainContent() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  MSHCheckbox(
-                    size: 32,
-                    value: isChecked,
-                    checkedColor: Color(0xff18DAA3),
-                    style: MSHCheckboxStyle.fillScaleCheck,
-                    onChanged: (selected) {
-                      setState(() {
-                        isChecked = selected;
-                      });
-                    },
-                  ),
-                  Text('با دوستام ورزش کنم')
-                ],
-              ),
-              Text('SECOND TEXT'),
-              Spacer(),
-              getTimeAndEditBadgs()
-            ],
-          ),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Image.asset('images/workout.png'),
-      ],
-    );
-  }
-
-  Row getTimeAndEditBadgs() {
-    return Row(
-      children: [
-        Container(
-          width: 90,
-          height: 28,
-          decoration: BoxDecoration(
-            color: Color(0xff18DAA3),
-            borderRadius: BorderRadius.circular(18),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
-            child: Row(
-              children: [
-                Text('10:30'),
-                SizedBox(
-                  width: 10,
-                ),
-                Image.asset('images/icon_time.png'),
-              ],
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 15,
-        ),
-        Container(
-          width: 95,
-          height: 28,
-          decoration: BoxDecoration(
-            color: Color(0xffE2F6F1),
-            borderRadius: BorderRadius.circular(18),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
-            child: Row(
-              children: [
-                Text(
-                  'ویرایش',
-                  style: TextStyle(color: Color(0xff18DAA3)),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Image.asset('images/icon_edit.png'),
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
---
+++ lib/task_widget.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:msh_checkbox/msh_checkbox.dart';
+import 'package:note_application/task.dart';
+
+class TaskWidget extends StatefulWidget {
+  TaskWidget({Key? key, required this.task}) : super(key: key);
+  Task task;
+  @override
+  State<TaskWidget> createState() => _TaskWidgetState();
+}
+
+class _TaskWidgetState extends State<TaskWidget> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return getTaskItem();
+  }
+
+  Container getTaskItem() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+      height: 132,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Color(0xffffffff),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: getMainContent(),
+      ),
+    );
+  }
+
+  Row getMainContent() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MSHCheckbox(
+                    size: 32,
+                    value: isChecked,
+                    checkedColor: Color(0xff18DAA3),
+                    style: MSHCheckboxStyle.fillScaleCheck,
+                    onChanged: (selected) {
+                      setState(() {
+                        isChecked = selected;
+                      });
+                    },
+                  ),
+                  Text(widget.task.title)
+                ],
+              ),
+              Text(widget.task.subTitle),
+              Spacer(),
+              getTimeAndEditBadgs()
+            ],
+          ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Image.asset('images/workout.png'),
+      ],
+    );
+  }
+
+  Row getTimeAndEditBadgs() {
+    return Row(
+      children: [
+        Container(
+          width: 90,
+          height: 28,
+          decoration: BoxDecoration(
+            color: Color(0xff18DAA3),
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+            child: Row(
+              children: [
+                Text('10:30'),
+                SizedBox(
+                  width: 10,
+                ),
+                Image.asset('images/icon_time.png'),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 15,
+        ),
+        Container(
+          width: 95,
+          height: 28,
+          decoration: BoxDecoration(
+            color: Color(0xffE2F6F1),
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+            child: Row(
+              children: [
+                Text(
+                  'ویرایش',
+                  style: TextStyle(color: Color(0xff18DAA3)),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Image.asset('images/icon_edit.png'),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
